573e45b2faba05c63ff1f77d7d0bac651ef5dc65
Lammii-Baanee <146845168+Lammii-Baanee@users.noreply.github.com>
2024-02-13 15:41:41 +0300

    Updates

---
+++ precache-manifest.ff6eb0d4c6c5721bc95c7217e3bcb34f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db854857f21534636082c6e6b51b302",
+    "url": "/updated-amazon-front-end/index.html"
+  },
+  {
+    "revision": "ae1901c7fb8346baf613",
+    "url": "/updated-amazon-front-end/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "728a808bd7f416e97dce",
+    "url": "/updated-amazon-front-end/static/css/main.f78c752c.chunk.css"
+  },
+  {
+    "revision": "ae1901c7fb8346baf613",
+    "url": "/updated-amazon-front-end/static/js/2.d540871e.chunk.js"
+  },
+  {
+    "revision": "728a808bd7f416e97dce",
+    "url": "/updated-amazon-front-end/static/js/main.7036103f.chunk.js"
+  },
+  {
+    "revision": "069459d7783f098d53af",
+    "url": "/updated-amazon-front-end/static/js/runtime~main.1fb34eb0.js"
+  },
+  {
+    "revision": "7510a07f4cef21519b235e47135f3d7a",
+    "url": "/updated-amazon-front-end/static/media/10001.7510a07f.jpg"
+  },
+  {
+    "revision": "ee2f8559d3274c20e796de5dfdbf3a35",
+    "url": "/updated-amazon-front-end/static/media/10002.ee2f8559.jpg"
+  },
+  {
+    "revision": "01b4924ba45dbf69da983ca7a606afa4",
+    "url": "/updated-amazon-front-end/static/media/10003.01b4924b.jpg"
+  },
+  {
+    "revision": "fb23a7ce4ddb424506c42264fa2799bc",
+    "url": "/updated-amazon-front-end/static/media/10004.fb23a7ce.jpg"
+  },
+  {
+    "revision": "195f53a6213b65947079891bb50b418c",
+    "url": "/updated-amazon-front-end/static/media/10005.195f53a6.jpg"
+  }
+]);
