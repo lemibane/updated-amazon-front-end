4e8a22ceadc3865642ddb48eaba604f61786b6d4
Lammii-Baanee <146845168+Lammii-Baanee@users.noreply.github.com>
2024-02-13 15:34:48 +0300

    m2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-gitimport { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import Routing from "./Router.jsx";
 import { auth } from "./Utility/firebase.js";
 import { DataContext } from "./Components/DataProvider/DataProvider.jsx";
 import { Type } from "./Utility/action.type.js";
-
 function App() {
   const [{ user }, dispatch] = useContext(DataContext);
-
   useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
+    auth.onAuthStateChanged(authUser => {
       if (authUser) {
         console.log(authUser);
         dispatch({
           type: Type.SET_USER,
-          user: authUser,
+          user: authUser
         });
       } else {
         dispatch({ type: Type.SET_USER, user: null });
@@ -22,5 +20,4 @@ function App() {
   }, []);
   return <Routing />;
 }
-
 export default App;
