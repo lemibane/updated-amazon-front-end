fdec2c6082b311a611a7c76800ff6db472a58322
Lammii-Baanee <146845168+Lammii-Baanee@users.noreply.github.com>
2024-02-13 15:36:40 +0300

    g

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useContext } from "react";
+import { react, useEffect, useState, useContext } from "react";
 import Routing from "./Router.jsx";
 import { auth } from "./Utility/firebase.js";
 import { DataContext } from "./Components/DataProvider/DataProvider.jsx";
